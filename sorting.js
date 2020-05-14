bcfc739a21519d8e8da63eeb9c92e7d202d90dd2
Catrinici <catrinici.antonina@gmail.com>
2020-05-14 16:13:56 -0700

    Sorting algorithms

---
+++ sorting.js
@@ -0,0 +1,47 @@
+//1. Bubblesort
+function bubbleSort(arr) {
+    for (var j = 0; j < arr.length; j++) {
+        for (var i = 0; i < arr.length - 1 - j; i++) {
+            if (arr[i] > arr[i + 1]) {
+                var temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+bubbleSort([5, 3, 1, 2]) //to return [1,2,3,5]
+bubbleSort([5, 4, 3, 2, 1]) //to return [1,2,3,4,5]
+bubbleSort([5, 3, 1, -1, -3]) //to return [-3,-1,1,3,5]
+bubbleSort([5, 3, 1]) //to return [1,3,5]
+
+/*********************************************************************/
+
+//2. SelectionSorts
+
+function selectionSort(arr) {
+    // Enter code below
+    for (var j = 0; j < arr.length; j++) {
+        var min = arr[j];
+        var min_index = j;
+
+        for (var i = j; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                min_index = i;
+            }
+        }
+        console.log("min of ", min, "was found at index", min_index);
+        var temp = arr[j];
+        arr[j] = min;
+        arr[min_index] = temp;
+    }
+    return arr;
+
+}
+
+
+selectionSort([5, 7, 0]) //to return [0,5,7]
+selectionSort([5, 7, 0, -10]) //to return [-10,0,5,7]
+selectionSort([5, 7, 0, -10, 4]) //to return [-10,0,4,5,7]
