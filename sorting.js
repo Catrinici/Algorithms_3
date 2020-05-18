12e230a992336fdfd1d89511ca52f83d957170e7
Catrinici <catrinici.antonina@gmail.com>
2020-05-18 09:58:14 -0700

    sorting

--- sorting.js
+++ sorting.js
@@ -45,3 +45,32 @@ function selectionSort(arr) {
 selectionSort([5, 7, 0]) //to return [0,5,7]
 selectionSort([5, 7, 0, -10]) //to return [-10,0,5,7]
 selectionSort([5, 7, 0, -10, 4]) //to return [-10,0,4,5,7]
+
+
+/*********************************************************************/
+
+//3. InsertionSort
+
+function insertionSort(arr) {
+
+    var current;
+    for (var i = 1; i < arr.length; i++) {
+        current = arr[i];
+
+        var j = i - 1;
+        while (j >= 0 && arr[j] > current) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = current;
+    }
+
+}
+
+insertionSort([5, 3, 1, 2]) //to return [1,2,3,5]
+insertionSort([5, 3, 1, 2, 9, -5]) //to return [-5,1,2,3,5,9]
+insertionSort([5, 3, 1, 2, 9, -5, 10, 13, 0]) //to return [-5,0,1,2,3,5,9,10,13]
+
+/*********************************************************************/
+
+//4.QuickSort
